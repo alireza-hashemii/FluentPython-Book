dfd53bb046054a1ca617e85fb03154cef3d282cc
1alirezahashemi1 <pyalireza7@gmail.com>
2023-09-13 09:47:08 +0330

    function-specific attributes page 148

--- chapter5 - first-class functions.py
+++ chapter5 - first-class functions.py
@@ -28,12 +28,62 @@
 # print(f)
 
 
-from functools import reduce
-from operator import add
+# from functools import reduce
+# from operator import add
 
-# two diffrent ways of adding numbers from 0 to 99
+# # two diffrent ways for adding numbers from 0 to 99
 
-print(reduce(add,range(100)))
+# print(reduce(add,range(100)))
 
-print(sum(range(100)))
+# print(sum(range(100)))
 
+# def if_odd(num:int,num2):
+#     return num * num2
+
+# f = reduce(if_odd,range(1,4))
+# print(f)
+
+
+# names = ["aireza","mohammad","sepide","maria","Jack","natalie"]
+
+# def inout(nm):
+#     if len(nm) > 5:
+#         return nm
+#     return None
+
+
+# print(list(map(inout,names)))
+
+
+# list = ["sepehr","Xaraza","abbass","pedram","sara","sophia","kiarash"]
+# print(sorted(list,key=lambda item: item[::-1]))
+
+# import random
+# from typing import Any
+
+# class BingoCage:
+#     def __init__(self,items):
+#         self._items = list(items)
+#         random.shuffle(self._items)
+    
+#     def pick(self):
+#         try:
+#             return self._items.pop()
+#         except IndexError:
+#             raise LookupError('Pick from vacant empty cage')
+        
+#     def __call__(self):
+#         return self.pick() 
+
+# first_bingo = BingoCage(range(7))
+# print(first_bingo())
+
+
+def fact(num):
+    return 1 if num < 2 else num * fact(num - 1)
+
+class A:
+    pass
+
+obj = A()
+print(set(dir(fact))  - set(dir(obj)))
