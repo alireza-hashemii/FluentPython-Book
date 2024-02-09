42292223397866c4f14c7b7c61fbc4ec416a335a
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-02-09 06:42:45 +0330

    assignment inside function

--- chapter 7 - decorators and closures.py
+++ chapter 7 - decorators and closures.py
@@ -14,29 +14,41 @@
 # s()
 
 
-registry = []
+# registry = []
 
-def register(func):
-    print(f'running register {func()}')
-    registry.append(func)
-    return func
+# def register(func):
+#     print(f'running register {func}')
+#     registry.append(func)
+#     return func
 
-@register
-def f1():
-    print('running f1()')
+# @register
+# def f1():
+#     print('running f1()')
 
-@register
-def f2():
-    print('running f2()')
+# @register
+# def f2():
+#     print('running f2()')
 
-@register
-def f3():
-    print('running f3()')
 
-def main():
-    print('running main()')
-    print('registery ->', registry)
+# def f3():
+#     print('running f3()')
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     print('running main()')
+#     print('registery ->', registry)
+#     f1()
+#     f2()
+#     f3()
 
+# if __name__ == "__main__":
+#     main()
+
+b = 6
+def fun1(a):
+    
+    print(a)
+    print(b)
+    b = 9
+
+fun1(3)
+print(b)
