754ed6751bb70702b66ca97d98db6df3699368fb
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-02 20:04:26 +0330

    exploring binary sequence types

--- chapter4 - text versus bytes.py
+++ chapter4 - text versus bytes.py
@@ -1,22 +1,32 @@
-# simple encoding and decoding
-d = "cafeη"
-print(d)
-dd = d.encode()
-print(dd)
-print(len(d))
-print(len(dd))
+# # simple encoding and decoding
+# d = "cafeη"
+# print(d)
+# dd = d.encode()
+# print(dd)
+# print(len(d))
+# print(len(dd))
 
-a = "alireza"
-c = b"alireza"
-b = a.encode("ASCII")
-if b == c:
-    print("Encoding succesful.")
+# a = "alireza"
+# c = b"alireza"
+# b = a.encode("ASCII")
+# if b == c:
+#     print("Encoding succesful.")
 
 
 
-d = "cafeη".encode(encoding="utf-8")
-print(d)
+# d = "cafeη".encode(encoding="utf-8")
+# print(d)
 
-print(d[0])
-print(d[:1])
+# print(d[0])
+# print(d[:1])
 
+
+# name = "welcome必"
+# by = bytes(name, encoding="utf_8")
+# # print(by[0])
+# # print(by[-1])
+# # print(len(by))
+
+
+# different ways of instanciating binary types
+f = bytes("hello", encoding="utf_8")
