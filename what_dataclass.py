9449140fb5d0e7f7604fbc0df63e56b66a76073f
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-12 14:37:02 +0330

    variable type anotation

--- what_dataclass.py
+++ what_dataclass.py
@@ -4,14 +4,14 @@ from typing import NamedTuple
 # we have 3 primary ways to create dataclasses. First 2 of them
 # are collections.namedtuple and typing.NamedTuple. Tiny differences 
 # could be spot between them. In addition, the last one is dataclass  
-# decorator provided by dataclasses module.
+# decorator provided by dataclasses  module.
 
 Point = namedtuple("Point", ["x", "y"])
 math_qa = Point(3,5)
-
 
 Book = namedtuple("Book", ["pages", "author", "sales"],defaults=["N/A","N/A","N/A"])
 favourite_book = Book()
 
 Perfume = namedtuple("Perfume", ["price", "capacity", "name"])
-perf = Perfume()
+perf = Perfume(34,250,"Dioe")
+perf.capacity
