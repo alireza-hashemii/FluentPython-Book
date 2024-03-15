4a5d57a8b0c80a460e5be6fa7ad0b2198e55df3d
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-15 11:59:18 +0330

    reducing functions

---
+++ chapter 7 - functions_fc_objects.py
@@ -0,0 +1,18 @@
+def factorial(n): 
+    """returns n!"""
+    return 1 if n < 2 else n * factorial(n - 1)
+
+using_map = list(map(factorial, range(5)))
+using_liscoms = [factorial(n) for n in range(5)] # output: [1, 1, 2, 6, 24]
+using_concomps =  [factorial(n) for n in range(6) if n % 2]
+
+
+from functools import reduce
+from operator import add
+# two ways of doing same work.
+summation_answer = reduce(add , range(100))
+second_way = sum(range(100))
+
+
+#other reducing functions
+any()
