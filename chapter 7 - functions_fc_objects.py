1068b49e6f679ebdf06485f32eed55117a4c4e03
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-15 11:42:55 +0330

    relative muteabillity of tuples

---
+++ chapter 7 - functions_fc_objects.py
@@ -0,0 +1,12 @@
+# def deco(func):
+#     def target():
+#         print("befor it runs")
+#         func()
+#         print("after it runs")
+        
+#     return target
+
+
+# @deco
+# def s():
+#     print('salam
