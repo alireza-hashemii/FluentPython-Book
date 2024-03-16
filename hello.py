38e26f5878999b8127ef38dd78016b9d03e6590b
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-16 15:54:42 +0330

    why is that a bad idea to put a mutable type as parameter default?

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-def reverse_sorting(dl):
-    sm = 0
-    for i in dl:
-        sm += i
-    return sm
+def add_to_list(num, items=None):
+    if items is None:
+        items = list()
+        items.append(num)
+        print(items)
+    else:
+        items.append(num)
+        print(items)
 
-
-
+add_to_list(5)
+add_to_list(10)
+add_to_list(15)
