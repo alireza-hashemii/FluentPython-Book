a308e0ce63cf247a0cc87f962bd7e61c6bf44712
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-18 07:35:26 +0330

    decorators and closures

---
+++ chapter8 - typehint_infunctions.py
@@ -0,0 +1,29 @@
+# class Bird:
+#     pass
+
+
+# class Duck(Bird): 
+#     def quack(self):
+#         print('Quack!')
+
+# def alert(birdie): 
+#     birdie.quack()
+
+# def alert_duck(birdie: Duck) -> None: 
+#     birdie.quack()
+
+
+# def alert_bird(birdie: Bird) -> None: 
+#     birdie.quack()
+
+
+# type hints for tuples
+
+from geolib import geohash as gh
+
+PRECISION = 9
+
+def geohash(lat_lon: tuple[float, float]) -> str: 
+    return gh.encode(*lat_lon, PRECISION)
+
+print(geohash((3632.4525,56517.8111)))
