6e2463b6adec2aea7cce051c2e03b308586fabed
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-03-19 16:24:39 +0330

    built-in decorators

--- chapter8 - typehint_infunctions.py
+++ chapter8 - typehint_infunctions.py
@@ -1,20 +1,20 @@
-# class Bird:
-#     pass
+class Bird:
+    pass
 
 
-# class Duck(Bird): 
-#     def quack(self):
-#         print('Quack!')
+class Duck(Bird): 
+    def quack(self):
+        print('Quack!')
 
-# def alert(birdie): 
-#     birdie.quack()
+def alert(birdie): 
+    birdie.quack()
 
-# def alert_duck(birdie: Duck) -> None: 
-#     birdie.quack()
+def alert_duck(birdie: Duck) -> None: 
+    birdie.quack()
 
 
-# def alert_bird(birdie: Bird) -> None: 
-#     birdie.quack()
+def alert_bird(birdie: Bird) -> None: 
+    birdie.quack()
 
 
 # type hints for tuples
