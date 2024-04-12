fc1108fc3f3904c7d7f0737eebee00df59195aa2
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-04-12 08:35:03 +0330

    multiple inheritance example

--- chapter14 - inheritance.py
+++ chapter14 - inheritance.py
@@ -15,3 +15,43 @@ class Bird(Animal):
         return super(Bird, self).__setitem__(name)
 
 # multiple inheritance and method resoltion order introduction
+class Root:
+
+    def ping(self):
+        print(f"{self}.ping() in Root")
+
+    def pong(self):
+        print(f"{self}.pong() in Root")
+
+    def __repr__(self) -> str:
+        cls_name = type(self).__name__
+        return f"<instance of {cls_name}>"
+
+
+class A(Root):
+    def ping(self):
+        print(f'{self}.ping() in A')
+        super().ping()
+
+    def pong(self):
+        print(f'{self}.pong() in A')
+        super().pong()
+
+
+class B(Root):
+    def ping(self):
+        print(f'{self}.ping() in B')
+        super().ping()
+
+    def pong(self):
+        print(f'{self}.pong() in B')
+
+
+class Leaf(A,B):
+    def ping(self):
+        print(f'{self}.ping() in Leaf')
+        super().ping()
+
+leaf1 = Leaf()
+# leaf1.ping()
+# leaf1.pong()
