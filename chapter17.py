bd083afe66d951d34cbbc3a8947392d8c2fe9222
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-05-09 19:20:09 +0330

    2 ways of iterating over an iterable - do it yourself or let python do it,.

--- chapter17.py
+++ chapter17.py
@@ -1,49 +1,68 @@
-# import re
-import reprlib
+# # import re
+# import reprlib
 
-# RE_WORD = re.compile(r'\w+')
+# # RE_WORD = re.compile(r'\w+')
 
 
-# class Sentence:
+# # class Sentence:
 
-#     def __init__(self, text) -> None:
-#        self.text = text
-#        self.words = RE_WORD.findall(text)
+# #     def __init__(self, text) -> None:
+# #        self.text = text
+# #        self.words = RE_WORD.findall(text)
 
-#     def __getitem__(self, index):
-#         return self.words[index]
+# #     def __getitem__(self, index):
+# #         return self.words[index]
 
+# #     def __len__(self):
+# #         return len(self.words) 
+    
+# #     def __repr__(self):
+# #         return "Sentence(%s)" % reprlib.repr(self.text)
+    
+
+# # s = Sentence("I have been waiting for this brother.")
+# # print(s)
+# # for i in s:
+# #     print(i)
+
+# class Econvert:
+#     def __init__(self, email:str) -> None:
+#         self.email = list(email)
+    
 #     def __len__(self):
-#         return len(self.words) 
-    
-#     def __repr__(self):
-#         return "Sentence(%s)" % reprlib.repr(self.text)
-    
+#         return len(self.email)
 
-# s = Sentence("I have been waiting for this brother.")
-# print(s)
+#     def __repr__(self):
+#         return "Econvert(%s)" % reprlib.repr(self.email)
+    
+#     def __iter__(self):
+#         pass
+    
+# eml = Econvert("myson_sepehr@gmail.com")
+
+# from collections import abc
+# try:
+#     iter(eml)
+# except TypeError:
+#     print("Not Iterable")
+
+# print(isinstance(eml, abc.Iterable))
+
+## 1 
+# s = "ABC"
+# for char in s:
+#     print(char)
+
+## 2
+# s ="ABC"
+# iter_s = iter(s)
+
+# while True:
+#     try:
+#         print(next(iter_s))
+#     except StopIteration:
+#         break
+
+# print("----")
 # for i in s:
 #     print(i)
-
-class Econvert:
-    def __init__(self, email:str) -> None:
-        self.email = list(email)
-    
-    def __len__(self):
-        return len(self.email)
-
-    def __repr__(self):
-        return "Econvert(%s)" % reprlib.repr(self.email)
-    
-    def __iter__(self):
-        pass
-    
-eml = Econvert("myson_sepehr@gmail.com")
-
-from collections import abc
-try:
-    iter(eml)
-except TypeError:
-    print("Not Iterable")
-
-print(isinstance(eml, abc.Iterable))
